6ea4b7046b796aaf9838bcd823f413946394b517
KamataRyo <mugil.cephalus+github@gmail.com>
2015-05-31 20:25:04 +0900

    script test

--- js/parseCSV.js
+++ js/parseCSV.js
@@ -1,1 +1,17 @@
 /*CSVパーサー*/
+var path ='./csv/colonies.csv';
+var target = "#dataset";
+var csvList;
+var insert = '';
+		$("body").append("<h3>aaa</h3>");
+
+$.ajax({
+	url: path,
+	success: function(data){
+	csvList = $.csv()(data);
+	for (var i = 1; i < csvList.length; i++) {
+		insert += "<li>" + csvList[i][0] + "</li>";
+	};
+	$(target).append(insert);
+	}
+});
