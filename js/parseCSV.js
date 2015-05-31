3b044d7b9e4508b88b36567a00a3298011bbff66
KamataRyo <mugil.cephalus+github@gmail.com>
2015-05-31 21:19:58 +0900

    some fix

--- js/parseCSV.js
+++ js/parseCSV.js
@@ -1,17 +1,7 @@
 /*CSVパーサー*/
 var path ='./csv/colonies.csv';
 var target = "#dataset";
-var csvList;
-var insert = '';
-		$("body").append("<h3>aaa</h3>");
+var req = new XMLHttpRequest();
 
-$.ajax({
-	url: path,
-	success: function(data){
-	csvList = $.csv()(data);
-	for (var i = 1; i < csvList.length; i++) {
-		insert += "<li>" + csvList[i][0] + "</li>";
-	};
-	$(target).append(insert);
-	}
-});
+req.open('GET',path, false);
+req.send(null);
---
+++ simplepush.sh
@@ -0,0 +1,4 @@
+#! /usr/bin
+git add .
+git commit -m "some fix"
+git push origin gh-pages
